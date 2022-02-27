04bc97560def7f0062f5c117c846dcce7ec3c4a9
Remnant <georgesimon4801@gmail.com>
2022-02-27 12:57:02 -0500

    inquirer init

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
-const fs = require('fs');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+// const pageHTML = generatePage(name, github);
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//   if (err) throw err;
   
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-//   console.log('================');
+inquirer
+.prompt([
+  {
+    type: 'input', 
+    name: 'name',
+    message: 'What is your name?',
+  }
+])
+.then(answers => console.log(answers));
 
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
+
+
 
 // printProfileData(profileDataArgs);
+
+
+// const printProfileData = profileDataArr => {
+  //   // This...
+  //   for (let i = 0; i < profileDataArr.length; i += 1) {
+    //     console.log(profileDataArr[i]);
+    //   }
+    
+    //   console.log('================');
+    
+    //   // Is the same as this...
+    //   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
 
 // const generatePage =() => 'Name: Jane, Github: Gsimon5841 ';
 // console.log(generatePage());
